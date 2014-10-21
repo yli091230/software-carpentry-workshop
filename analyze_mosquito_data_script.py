cfa6e885fbb0aa8efabded989aaf7253c5a21377
Yang LI <yli091230@gmail.com>
2014-10-21 14:22:42 -0700

    save as  a variable name

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,20 +1,23 @@
+import sys
 import pandas as pd
 import analyze_mosquito_data_lib as mosquito_lib 
 
-filename = 'A1_mosquito_data.csv'
+
+filename = sys.argv[1]
+
+print 'analyze data', filename
 
 #read the data
 data = pd.read_csv(filename)
-print data.head()
 
 #convert temperature to celsius
 data['temperature']=mosquito_lib.fahr_to_celsius(data['temperature'])
-print data.head()
+
 #analyze data
-parameters = mosquito_lib.analyze(data,filename)
+parameters = mosquito_lib.analyze(data,filename.replace('csv','png'))
 
 #save parameters to file
-parameters.to_csv('parameters.csv')
+parameters.to_csv(filename.replace('data','parameters'))
 
 print parameters
 
